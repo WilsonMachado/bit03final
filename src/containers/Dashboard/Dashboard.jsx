06cbfedc8d3a4023927229c0c5f546b478f141ed
WilsonMachado <wilson.machado@colbits.com.co>
2023-06-12 23:36:24 -0500

    Se agrega un padding mayor para el contenido principal. El siguiente paso es crear las cards para mostrar el contenido

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,7 +1,30 @@
+import { Box,  Typography } from '@mui/material'
 import React from 'react'
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+      <>
+      <Typography sx={StyleSheet.pageTitle} variant='h5'>Dashboard</Typography>
+      
+      <Box sx={styles.columnContainer}>
+
+
+
+      </Box>
+      
+      </>
+    
+    )
 }
+
+/** @type {import("@mui/material")} */
+
+const styles = {
+    pageTitle: {
+        mb: 2 
+    },   
+    columnContainer: {
+        columns: '280px 3',
+        maxWidth: 1440
+    }
+  }
