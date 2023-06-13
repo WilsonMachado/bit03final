5236c442d2f3bf548b6f450735361bffb272600c
WilsonMachado <wilson.machado@colbits.com.co>
2023-06-12 21:36:38 -0500

    Se empieza a dar forma al NavBar. También se establecen las bases de la metodología a usar para realizar los estilos

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,12 +1,36 @@
-import { Box, Typography } from '@mui/material'
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const AppHeader = () => {
   return (
-    <Box>
-        <Typography>
-            AppHeader
-        </Typography>
-    </Box>
-  )
+      <AppBar position='sticky' sx={styles.appBar}>
+          <Toolbar>
+            <IconButton onClick={() => console.log('Abriendo menú')} color="secondary">
+                <MenuIcon />
+            </IconButton>
+            <Box component='img' sx={styles.appLogo} src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/e-commerce-online-shop-design-template-aa02d6dc00d6567d0408bebdb3e5fab3.jpg'></Box>
+            <IconButton onClick={() => console.log('Logout')} color="secondary">
+                <LogoutIcon />
+            </IconButton>
+          </Toolbar>
+      </AppBar>
+      )
+}
+        
+
+/** @type {import("@mui/material")} */
+
+const styles = {
+    appBar: {
+        bgcolor: 'neutral.main' 
+    },
+    appLogo: {
+        borderRadius: 100,
+        width: 0.3,
+        ml: 5,
+        mr: 5,
+        cursor: 'pointer'
+    }
 }
